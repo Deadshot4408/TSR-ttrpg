1552b21039a0aa1e65457ab47e427ab6278fd8b2
Deadshot4408 <jboz1188@gmail.com>
2024-12-25 23:39:49 -0500

    Add Locations

--- world.py
+++ world.py
@@ -1,1 +1,12 @@
 # Locations and Map Logic
+class Location:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.connections = {}
+
+    def connect(self, direction, location):
+        self.connections[direction] = location
+
+    def describe(self):
+        return f"{self.name}\n{self.description}"
